94bf6beec588126366aeadafc1dbf73da212311f
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-10-20 20:08:10 -0300

    added login controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,7 @@
-import { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
-import { User } from '../models/User';
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { User } from "../models/User";
+import jwt from "jsonwebtoken";
 
 export const register = async (req: Request, res: Response): Promise<void> => {
   const { email, password, role, name, surname } = req.body;
@@ -8,12 +9,12 @@ export const register = async (req: Request, res: Response): Promise<void> => {
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      res.status(400).json({ message: 'El correo ya está registrado.' });
+      res.status(400).json({ message: "El correo ya está registrado." });
       return;
     }
 
-    if (!['admin', 'operator'].includes(role)) {
-      res.status(400).json({ message: 'Rol no válido.' });
+    if (!["admin", "operator"].includes(role)) {
+      res.status(400).json({ message: "Rol no válido." });
       return;
     }
 
@@ -24,13 +25,45 @@ export const register = async (req: Request, res: Response): Promise<void> => {
       password: hashedPassword,
       role,
       name,
-      surname
+      surname,
     });
 
     await user.save();
 
-    res.status(201).json({ message: 'Usuario registrado exitosamente.' });
+    res.status(201).json({ message: "Usuario registrado exitosamente." });
   } catch (error) {
-    res.status(500).json({ message: 'Error en el servidor.' });
+    res.status(500).json({ message: "Error en el servidor." });
+  }
+};
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(400).json({ message: "Credenciales inválidas." });
+      return;
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      res.status(400).json({ message: "Credenciales inválidas." });
+      return;
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "7d" }
+    );
+
+    res.status(200).json({
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error en el servidor." });
   }
 };
